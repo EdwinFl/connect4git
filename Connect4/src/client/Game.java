c281b3f0f9aee748bf7804f196feda2e56a9232d
RoyNijhuis <roynijhuis@hotmail.nl>
2014-11-17 13:44:19 +0100

    teams added

--- Connect4/src/client/Game.java
+++ Connect4/src/client/Game.java
@@ -5,24 +5,31 @@ import java.util.Scanner;
 
 public class Game {
 	private Board board;
-	public boolean RUNNING=true;
-	private boolean won=false;
+	public boolean running;
+	private boolean won;
 	private Scanner scanner;
 	private ArrayList<Player> players;
+	private Team team1;
+	private Team team2;
 	
 	public Game()
 	{
+		running = true;
+		won=false;
 		scanner = new Scanner(System.in);
 		players = new ArrayList<Player>();
 		players.add(new Player("Henkie"));
 		players.add(new Player("Jaap"));
+		players.add(new Player("Henkie2"));
+		players.add(new Player("Jaap2"));
+		
 		TUI tui = new TUI();
 		board = new Board();
 		tui.showWelcomeMessage();
 		tui.showBoard(board);
 		int input=0;
 		
-		while(RUNNING)
+		while(running)
 		{
 			for(Player player:players)
 			{
@@ -30,13 +37,13 @@ public class Game {
 				input = scanner.nextInt();
 				board.addDisc(input, player);
 				tui.showBoard(board);
-				RUNNING = !board.checkWin(player);
-				if(!RUNNING)
+				running = !board.checkWin(player);
+				if(!running)
 				{
 					break;
 				}
 			}
-			RUNNING = !board.checkDraw();
+			running = !board.checkDraw();
 		}
 	}
 	
